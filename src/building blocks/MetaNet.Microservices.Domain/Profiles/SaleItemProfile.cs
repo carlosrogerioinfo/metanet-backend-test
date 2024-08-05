f9d25aa70c66385df00c4129bb770757208559da
Carlos Rogerio <carlosrogerio.info@gmail.com>
2024-08-05 10:41:12 -0300

    Fix

--- src/building blocks/MetaNet.Microservices.Domain/Profiles/SaleItemProfile.cs
+++ src/building blocks/MetaNet.Microservices.Domain/Profiles/SaleItemProfile.cs
@@ -9,7 +9,6 @@ namespace MetaNet.Microservices.Domain.Profiles
         public SaleItemProfile()
         {
             CreateMap<SaleItem, SaleItemResponse>()
-                //.ForMember(dest => dest.AccountType, opt => opt.MapFrom(src => StringExtensionTools.GetDescriptionFromEnum(src.AccountType)))
                 .ForMember(dest => dest.Product, opt => opt.MapFrom(src => src.Product))
                 .ReverseMap();
         }
